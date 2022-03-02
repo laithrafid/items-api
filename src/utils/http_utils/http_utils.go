1e36855b7cc9eea1c0e0780d13244b770067c50e
Laith <laith@bayt.cloud>
2022-03-02 03:09:50 -0500

    utis

---
+++ src/utils/http_utils/http_utils.go
@@ -0,0 +1,18 @@
+package http_utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/laithrafid/bookstore_items-api/src/utils/errors_utils"
+)
+
+func RespondJson(w http.ResponseWriter, statusCode int, body interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	json.NewEncoder(w).Encode(body)
+}
+
+func RespondError(w http.ResponseWriter, err errors_utils.RestErr) {
+	RespondJson(w, err.Status(), err)
+}
